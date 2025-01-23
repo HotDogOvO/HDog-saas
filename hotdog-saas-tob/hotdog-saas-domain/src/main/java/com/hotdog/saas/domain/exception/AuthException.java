4de784c68177c79996f1bcd554abfb11c0157f0f
HotDog <wudonghe1996@gmail.com>
2025-01-23 17:12:06 +0800

    开启token验证

--- hotdog-saas-tob/hotdog-saas-domain/src/main/java/com/hotdog/saas/domain/exception/AuthException.java
+++ hotdog-saas-tob/hotdog-saas-domain/src/main/java/com/hotdog/saas/domain/exception/AuthException.java
@@ -1,29 +1,28 @@
 package com.hotdog.saas.domain.exception;
 
-import com.hotdog.saas.domain.constant.Constants;
 import com.hotdog.saas.domain.enums.ResultCodeEnum;
 
 import lombok.Getter;
 
 @Getter
-public class BusinessException extends RuntimeException {
+public class AuthException extends RuntimeException {
 
     private final ResultCodeEnum resultCode;
     private final String message;
 
-    public BusinessException(ResultCodeEnum resultCodeEnum) {
+    public AuthException(ResultCodeEnum resultCodeEnum) {
         super(resultCodeEnum.getCode());
         this.resultCode = resultCodeEnum;
         this.message = resultCodeEnum.getMessage();
     }
 
-    public BusinessException(ResultCodeEnum resultCodeEnum, String message) {
+    public AuthException(ResultCodeEnum resultCodeEnum, String message) {
         super(resultCodeEnum.getCode());
         this.resultCode = resultCodeEnum;
         this.message = message;
     }
 
-    public BusinessException(String message) {
+    public AuthException(String message) {
         super(ResultCodeEnum.FAIL.getCode());
         this.message = message;
         this.resultCode = ResultCodeEnum.FAIL;
--- hotdog-saas-tob/hotdog-saas-web/src/main/java/com/hotdog/saas/web/interceptor/TokenInterceptor.java
+++ hotdog-saas-tob/hotdog-saas-web/src/main/java/com/hotdog/saas/web/interceptor/TokenInterceptor.java
@@ -1,8 +1,10 @@
 package com.hotdog.saas.web.interceptor;
 
 import com.hotdog.saas.domain.enums.ResultCodeEnum;
+import com.hotdog.saas.domain.exception.AuthException;
 import com.hotdog.saas.domain.exception.BusinessException;
 import com.hotdog.saas.domain.foundation.AuthService;
+import com.hotdog.saas.domain.utils.HttpUtils;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -20,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
  * <li>token校验</li>
  * <li>权限校验</li>
  *
- * @author donghe.wu.o
+ * @author hotdog
  * @date 2024/12/16 17:39
  */
 @Slf4j
@@ -39,22 +41,25 @@ public class TokenInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         try {
             if (needValidToken) {
-                String token = authService.extractToken();
-                if (StringUtils.isEmpty(token)) {
-                    throw new BusinessException("token为空");
+                // post请求才进行校验，目前只开放Post、Options两种请求
+                if (HttpUtils.validHttpPost(request.getMethod())) {
+                    String token = authService.extractToken();
+                    if (StringUtils.isEmpty(token)) {
+                        throw new AuthException("token为空");
+                    }
+                    authService.verifyToken(token);
                 }
-                authService.verifyToken(token);
             }
             return true;
         } catch (ExpiredJwtException e) {
-            throw new BusinessException(ResultCodeEnum.TOKEN_EXPIRE);
+            throw new AuthException(ResultCodeEnum.TOKEN_EXPIRE);
         } catch (SignatureException e) {
-            throw new BusinessException(ResultCodeEnum.TOKEN_INVALID);
+            throw new AuthException(ResultCodeEnum.TOKEN_INVALID);
         } catch (IllegalArgumentException | MalformedJwtException | UnsupportedJwtException e) {
-            throw new BusinessException(ResultCodeEnum.TOKEN_FAIL);
+            throw new AuthException(ResultCodeEnum.TOKEN_FAIL);
         } catch (Exception e) {
-            if (e instanceof BusinessException) {
-                throw (BusinessException) e;
+            if (e instanceof AuthException) {
+                throw (AuthException) e;
             }
             log.error("token拦截器处理异常，{}", e.getMessage(), e);
             throw new BusinessException(ResultCodeEnum.FAIL);
